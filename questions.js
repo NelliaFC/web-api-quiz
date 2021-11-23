2d25e74d86fbd6f5abbb48422663ec0dec78a04f
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-22 21:34:12 -0600

    timer function

---
+++ questions.js
@@ -0,0 +1,20 @@
+/*setting timer*/
+
+var timer = 60;
+var timeCount;
+
+/*set up timer to count off from the start of the quiz*/
+
+function setupTimer() {
+    timeCount = setInterval(function () {
+        timer--;
+        var timeReset = timeElement.textContent = "Time:" + " " + timer;
+       timer = timer;
+        if (timer <= 0) {         
+            clearInterval(timeCount);
+              
+            timeElement.textContent = timeReset;
+             
+        }
+    }, 1000)
+}
