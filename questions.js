7045c6120df7982ec2d90d3395a953f622c076c1
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-22 21:54:34 -0600

    final score function

--- questions.js
+++ questions.js
@@ -29,4 +29,56 @@ document. addEventListener('click', function (event) {
 })
 
 /*On click handler function*/
-/*
+/*Create a function to display each question on click of the button*/
+
+var i = 0;
+function onclickHandler(event) {
+    if(timer<=0){
+        clearInterval(timeCount);
+        divContEL.style.display="none";
+        displayResult();
+    }
+    var answerText = event.target.textContent 
+    if (answerText === questions[i].answer) {
+        timer = timer;
+        responsDiv.setAttribute("style", "color: green")
+        responsDiv.textContent = "Correct";
+    } else {
+
+        responsDiv.setAttribute("style", "color: red")
+        responsDiv.textContent = "Wrong";
+        timer = timer - 15;
+     }
+    
+     if  (i<questions.length-1) {
+        i++;
+
+        setTimeout(function () {
+        displayQuestions();
+        responsDiv.textContent = "";
+      }, 1000)
+      }else {
+          setTimeout(function () {
+              responsDiv.textContent = "";
+              displayResult();
+              clearInterval(timeCount);
+            
+          }, 500)
+      
+  
+          divContEL.innerHTML = '';
+     }
+     /*Create a function for final score */
+ 
+ function displayResult() {
+    finishDiv.style.visibility = "visible";
+    timeElement.textContent = "Time:" + " " + timer;
+    var HighScores = timer;
+    localStorage.getItem(HighScores)
+    finalScore.textContent = "Your finally score is: " + HighScores;
+     localStorage.setItem("HighScores", HighScores)
+
+}
+}
+
+   
